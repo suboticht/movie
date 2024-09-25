ca97f798922f0f5dc63fb580b1e4feac4620b095
suboticht <86337073+suboticht@users.noreply.github.com>
2024-09-25 16:30:07 +0700

    Add files via upload

--- src/api/seriesData.tsx
+++ src/api/seriesData.tsx
@@ -2,16 +2,16 @@ import { Film } from "@/app/page";
 import axiosInstance from "./axiosConfig";
 import { AxiosResponse } from "axios";
 
-const seriesData = async(slug:string, page:number = 1, limit:number): Promise<Film[] | undefined> => {
+const seriesData = async(slug:string, page:number = 1, limit:number): Promise<any> => {
     
     try {
-        const response: AxiosResponse<Film[]> = await axiosInstance.get(`v1/api/danh-sach/${slug}`, {
+        const response: AxiosResponse<any> = await axiosInstance.get(`v1/api/danh-sach/${slug}`, {
             params: {
                 page: page,
                 limit: limit
             }
         })
-        return response.data.data
+        return response.data?.data
     } catch (err) {
         console.log(err);
         
